00643321b73f88791a4ed6abdc4dec8ef0b959a2
loonlylokly <loonlylokly@users.noreply.github.com>
2025-04-05 12:40:08 +0000

    deploy: cd93af607b4196e0044c6c7fe344a75b9838a360

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+(() => {document.addEventListener('DOMContentLoaded', () => {
+
+  // src/assets/js/modules/header.js
+  console.log("test");
+
+  // src/assets/js/index.js
+  console.log("test");
+
+});
+})();
